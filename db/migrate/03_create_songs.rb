07cb92d99fcd4f8d4b68ffee5331e7e8f28aa1fa
Bry Cox <carl.cc@gmail.com>
2019-05-20 21:32:57 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,5 +1,5 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-  
+
   def change
     create_table :songs do |t|
       t.string :name
@@ -7,5 +7,5 @@ class CreateSongs < ActiveRecord::Migration[4.2]
       t.integer :genre_id
     end
   end
-  
+
 end
